bf8c9859e2849e8b4ccfdc66a528439d384da2e7
noturnoooo <161738103+noturnoooo@users.noreply.github.com>
2024-05-16 12:16:21 -0300

    nrb

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T6:50:20");
+const tempoObjetivo2 = new Date("2024-12-05T5:40:65");
+const tempoObjetivo3 = new Date("2024-12-30T3:30:45");
+const tempoObjetivo4 = new Date("2024-02-01T1:20:30");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
